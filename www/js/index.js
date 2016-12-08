0fb5263189d5dbee3fa762a7ec6dbf8b2082d576
Mauj15 <mauj1_15@uni.worc.ac.uk>
2016-12-08 11:46:02 +0000

    Getting food

--- www/js/index.js
+++ www/js/index.js
@@ -47,7 +47,11 @@ function createDialog() {
         	
 function dialogDismissed(buttonIndex) {
 	
-	if(buttonIndex==1) new Toast({content: "yeah boi", duration: 3000});
+	if(buttonIndex==1) createNotification2()
+        
+        
+        
+        //new Toast({content: "yeah boi", duration: 3000});
    	else if(buttonIndex==2) new Toast({content: 'smh fam', duration: 3000});
 
 }
@@ -70,6 +74,27 @@ function createNotification() {
     	id: 		1,
         title: 		"Hey you",
         message: 	"This is an example notification",
+        date: 		notificationTime, 
+        badge: 		notification_count++
+   	});
+    
+}
+function createNotification2() {
+        		
+	//
+    //generate a time to post notification
+    //
+    var currentTime = new Date().getTime(); //current time
+    var notificationTime = new Date(currentTime + 30000); //delayed time  - add 1 second
+    			
+    //
+    //setup notification
+    //
+    
+    cordova.plugins.notification.local.schedule({ 
+    	id: 		1,
+        title: 		"You are hungry",
+        message: 	"Get some food",
         date: 		notificationTime, 
         badge: 		notification_count++
    	});
